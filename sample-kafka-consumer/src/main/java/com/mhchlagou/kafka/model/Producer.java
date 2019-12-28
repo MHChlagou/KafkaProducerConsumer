b0e1236cf2c30ef8bee5200056ed96d4ab22ddef
MHChlagou <chlagoumedhedi@outlook.com>
2019-12-28 01:19:36 +0100

    Retrive DB data to be used by the Producer and consumed by the Consumer

--- sample-kafka-consumer/src/main/java/com/mhchlagou/kafka/model/Producer.java
+++ sample-kafka-consumer/src/main/java/com/mhchlagou/kafka/model/Producer.java
@@ -1,20 +1,35 @@
 package com.mhchlagou.kafka.model;
 
+
 public class Producer {
 	
+	private Long idProducer;
 	private String name;
 	private String dept;
 	private Long salary;
 	
+	
+	
 	public Producer() {
 		super();
 	}
+	
+	
 
-	public Producer(String name, String dept, Long salary) {
+	public Producer(Long idProducer, String name, String dept, Long salary) {
 		super();
+		this.idProducer = idProducer;
 		this.name = name;
 		this.dept = dept;
 		this.salary = salary;
+	}
+
+	public Long getIdProducer() {
+		return idProducer;
+	}
+
+	public void setIdProducer(Long idProducer) {
+		this.idProducer = idProducer;
 	}
 
 	public String getName() {
---
+++ sample-kafka-producer/src/main/java/com/mhchlagou/kafka/sender/ProducerController.java
@@ -0,0 +1,62 @@
+package com.mhchlagou.kafka.sender;
+
+import java.util.List;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mhchlagou.kafka.model.Producer;
+import com.mhchlagou.kafka.repository.UserJpaRepository;
+
+@RestController
+@RequestMapping("kafka")
+public class ProducerController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProducerController.class);
+	
+	private String TOPIC = "AutoCreateTopics";
+	
+	@Autowired
+	private KafkaTemplate<String, Producer> kafkaTemplate;
+	
+	//-------------------------------- ADD Data to H2 DB -----------------------------------------
+	
+	@Autowired
+	private UserJpaRepository userJpaRepository;
+	
+	@PostMapping(value = "/load")
+	public Producer loadProducer(@RequestBody Producer producer) {
+		userJpaRepository.save(producer);
+		return userJpaRepository.findByName(producer.getName());
+	}
+	
+	//--------------------------------------------------------------------------------------------
+	
+	
+	@Bean
+	public NewTopic defaultTopic() {
+		return new NewTopic(TOPIC, 1, (short)1);
+	}
+	
+	@GetMapping(value = "/publish/all")
+	public String send() {
+		
+		List<Producer> producers = userJpaRepository.findAll();
+		if(producers != null) {
+			for(Producer producer : producers) {
+				kafkaTemplate.send(TOPIC, producer);
+				logger.info(String.format("************** Producer Data ************** ->", producer));
+			}
+		}
+		return "Message was published successfully to the topic : " + TOPIC;
+	}
+
+}
